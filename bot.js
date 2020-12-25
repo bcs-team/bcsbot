4b735fd76a7b21cc485e22ad4d67de9187e104b3
bcsbrawlball <76595239+bcsbrawlball@users.noreply.github.com>
2020-12-25 22:40:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,17 +22,17 @@ client.on("guildMemberAdd", member => {
     if (!channel) return 
       number = 10
   
-  var images = ["https://i.ibb.co/bKYSYVP/Untitled-1.png"];
+  var images = ["https://i.ibb.co/FBPC54c/Untitled-1.png"];
   var image = Math.floor(Math.random() * images.length);
   
 
 
-      var bbmsg = [ `A very warm welcome to you! It is lovely to have you among us! \n\n ${member}`,
-      `We are delighted to have you among us \n\n ${member}`,
-      `Welcome to the team!\n\n ${member} :wink: `,
-      `Well well well look who is here Guys \n\n ${member}`,
-      `Welcome aboard new buddy! \n\n ${member}`,
-      `On behalf of all of us, Welcome onboard!\n\n ${member}`]
+      var bbmsg = [ `A very warm welcome to you! It is lovely to have you among us! ${member}`,
+      `We are delighted to have you among us ${member}`,
+      `Welcome to the team! ${member} :wink: `,
+      `Well well well look who is here Guys ${member}`,
+      `Welcome aboard new buddy! ${member}`,
+      `On behalf of all of us, Welcome onboard! ${member}`]
 
     var bbcolor = [ "#FF0000","#0000FF","#000000","#FFFF00","#FFFFFF","#800080","#008000","#808080","#ADD8E6"]
     
@@ -42,7 +42,7 @@ client.on("guildMemberAdd", member => {
       var bbrandom = Math.floor(Math.random() * bbmsg.length)
     var bbchoose = bbmsg[bbrandom]
     let embed = new Discord.MessageEmbed()
-          .setTitle("_______"+member.user.username+"_______"+"\n```has joined Brawlball Community server```")
+          .setTitle(member.user.username+"```has joined Brawlball Community server```")
           .setColor(bbclrchos)
           .setThumbnail(member.user.avatarURL())
           .setDescription(bbchoose)
