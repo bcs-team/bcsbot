03ea734adbb92e18acf981a1d1f3a567b57052da
bcsbrawlball <76595239+bcsbrawlball@users.noreply.github.com>
2020-12-25 22:50:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,14 +11,14 @@ client.on('ready' , () => {
 })
 
 client.on("guildMemberAdd", member => {
-  const channel2 = member.guild.channels.cache.get("691026608777330808")
+  const channel2 = member.guild.channels.cache.get("789692987914584107")
   channel2.send(`${member} ** Welcome to Brawlball Community server **`)
 })
 
     
 
   client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.get("691026608777330808")
+    const channel = member.guild.channels.cache.get("789706902577348618")
     if (!channel) return 
       number = 10
   
