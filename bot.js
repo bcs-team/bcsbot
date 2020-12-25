3ba85e722c33cb48da6c9c03256e65d07bd8e3f9
bcsbrawlball <76595239+bcsbrawlball@users.noreply.github.com>
2020-12-25 22:00:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ client.on("guildMemberAdd", member => {
     let embed = new Discord.MessageEmbed()
           .setTitle(member.user.username+"  -  "+"has joined Brawlball Community server")
           .setColor(bbclrchos)
-          .setThumbnail(member.user.avatar)
+          .setThumbnail(member.avatarURL)
           .setDescription(bbchoose)
           .setImage(String([images[image]]))
           
