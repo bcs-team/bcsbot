ce958f8fe0294bca6a34de6822ed0c17d087fe09
bcsbrawlball <76595239+bcsbrawlball@users.noreply.github.com>
2020-12-26 08:45:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,34 @@ client.on('ready' , () => {
 
   client.user.setActivity("Brawlball")
 })
+client.on('message', message => {
+  if (message.content === '!dguide') {
+      const embed = new RichEmbed()
+
+          .setTitle('Our Style Brawlball Clan ')
+          .setColor(0xf60af8)
+          .setThumbnail(client.user.avatarURL)
+          .setDescription('Here we are friends who where gathered by the love of brawlball. There are no leaders, just players')
+          .addField('Teams', 'Runner - Supporter - Defender \n Substitute', false)
+          .setFooter('We are Our style and we will make brawlball great again', 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiR7KXpsrfkAhUl5uAKHf1DCLMQjRx6BAgBEAQ&url=https%3A%2F%2Fwallpaperstock.net%2Fs-logo-design-wallpapers_w35374.html&psig=AOvVaw3rIPgPX0g2OY5GqmOi0ph1&ust=1567694238587809%27')
+    message.channel.send(embed)
+  }
+})
+client.on('message', message => {
+  if (message.content === '!help') {
+      const embed = new RichEmbed()
+
+          .setTitle('Brawlball Community Bot Commands')
+          .setColor(0xf60af8)
+          .setThumbnail(client.user.avatarURL)
+          .setDescription('Here is the codes you can use in this bot')
+          .addField('!rguide', 'Showing Runner Guide for brawlball', false)
+          .addField('!sguide', 'Showing supporter Guide for brawlball', false)
+          .addField('!dguide', 'Showing Defender Guide for brawlball', false)
+          .setFooter('BCS', client.user.avatarURL )
+    message.channel.send(embed)
+  }
+})
 
 client.on("guildMemberAdd", member => {
   const channel2 = member.guild.channels.cache.get("691026608777330808")
