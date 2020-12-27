d4edd0ab03acc198b0e9c2e3f6abc7a6665f0968
bcsbrawlball <76595239+bcsbrawlball@users.noreply.github.com>
2020-12-27 03:28:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('message', message => {
           .setColor("#000000")
           .setThumbnail(client.user.avatarURL)
           .setDescription('https://docs.google.com/document/d/1N8ZLI5BwNHCE-lb3LmrtXpn53qHhpMnAnkOtxDOybbQ/edit?usp=sharing')
-          .setImage('https://i.ibb.co/Jcr8dqf/runner2.png')
+          .setImage('https://i.ibb.co/R3fVWh6/run2.png')
           .setFooter('Brawlball Community Server', client.user.avatarURL())
     message.channel.send(embed)
   }
@@ -56,7 +56,7 @@ client.on('message', message => {
           .setColor("#000000")
           .setThumbnail(client.user.avatarURL)
           .setDescription('https://docs.google.com/document/d/1N8ZLI5BwNHCE-lb3LmrtXpn53qHhpMnAnkOtxDOybbQ/edit?usp=sharing')
-          .setImage('https://i.ibb.co/Jcr8dqf/runner2.png')
+          .setImage('https://i.ibb.co/R3fVWh6/run2.png')
           .setFooter('Brawlball Community Server', client.user.avatarURL())
     message.channel.send(embed)
   }
