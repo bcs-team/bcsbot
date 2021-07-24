7e7ab64dc04626f487e64b694e62d67fdb389679
astrulmr <52928390+astrulmr@users.noreply.github.com>
2021-07-24 14:59:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,7 +146,7 @@ client.on('message', message => {
 client.on('ready', () => {
   client.guilds.cache.get('789692987143094293').channels.cache.get('793193064931655690').messages.fetch('793197949491609641');
   client.guilds.cache.get('789692987143094293').channels.cache.get('793193064931655690').messages.fetch('867598495518556190');
-
+  client.guilds.cache.get('789692987143094293').channels.cache.get('793193064931655690').messages.fetch('868462174408933416');
 })
 
 //-------------------------------------eu brawlball reaction-----------------------------------------
